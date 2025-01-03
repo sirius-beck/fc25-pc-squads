605600b0fd818d789c6d038bced835bad642a5cc
Sirius <sirius.red@outlook.com>
2025-01-03 10:37:45 -0300

    feat(scripts): Update squad downloader for skip non-PC platforms

--- scripts/download-squads.sh
+++ scripts/download-squads.sh
@@ -1,8 +1,10 @@
 git clone https://github.com/xAranaktu/FIFASquadFileDownloader downloader
 
 if ! cd downloader; then
-  echo "Failed to clone the repository."
-  exit 1
+	echo "Failed to clone the repository."
+	exit 1
 fi
+
+sed -i 's/if platform\["name"\] == "sta":/if platform\["name"\] != "pc64":/' main.py
 
 python3 main.py
