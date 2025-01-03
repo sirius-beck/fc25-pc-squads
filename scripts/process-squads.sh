a9f5b14f328c3cd509d5223020c8d5b62fc9cc4c
Sirius <sirius.red@outlook.com>
2025-01-03 07:46:29 -0300

    feat(scripts): Enhance process-squads.sh with version tracking

--- scripts/process-squads.sh
+++ scripts/process-squads.sh
@@ -7,12 +7,17 @@ if [ ! -d "$RESULT_FOLDER" ]; then
   exit 1
 fi
 
+if ! [ -d version ]; then
+  mkdir version
+fi
+
 # Process FUT folder
 FUT_SQUADS_FOLDER=$(ls -d $RESULT_FOLDER/FUT/*/)
 FUT_SQUADS_VERSION=$(basename "$FUT_SQUADS_FOLDER")
 FUT_SQUADS_FILES=$(find "$FUT_SQUADS_FOLDER" -type f ! -name "*.bin")
 zip -j fut-squads.zip "$FUT_SQUADS_FILES"
 echo "FUT_SQUADS_VERSION=$FUT_SQUADS_VERSION" >> "$GITHUB_ENV"
+echo "$FUT_SQUADS_VERSION" > version/fut-squads.version
 
 # Process squads folder
 SQUADS_FOLDER=$(ls -d $RESULT_FOLDER/squads/*/)
@@ -20,3 +25,4 @@ SQUADS_VERSION=$(basename "$SQUADS_FOLDER")
 SQUADS_FILES=$(find "$SQUADS_FOLDER" -type f ! -name "*.bin")
 zip -j squads.zip "$SQUADS_FILES"
 echo "SQUADS_VERSION=$SQUADS_VERSION" >> "$GITHUB_ENV"
+echo "$SQUADS_VERSION" > version/squads.version
