31092e27d910ab42a17d9ce7e6f124155b67c467
Sirius <sirius.red@outlook.com>
2025-01-03 06:45:47 -0300

    feat(scripts): enhance create-release.sh script
    Improve error handling, input validation, and overall script structure for create-release.sh

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-tag=$1 # "squads" or "fut-squads"
-version=$2
-release_exists=$(gh release view $tag-$version || echo "not_found")
+# Check if the correct number of arguments is provided
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <tag> <version>"
+  exit 1
+fi
 
-echo "tag: $tag | version: $version"
+# tag must be either "squads" or "fut-squads"
+if [ "$1" != "squads" ] && [ "$1" != "fut-squads" ]; then
+  echo "Invalid tag. Please use 'squads' or 'fut-squads'."
+  exit 1
+fi
 
-if [ "$release_exists" == "not_found" ]; then
-    gh release create $tag-$version $tag.zip -t "$tag v$version" -n "Downloaded $tag for version $version."
+# version must be a number
+if ! [[ "$2" =~ ^[0-9]+$ ]]; then
+  echo "Invalid version. Please provide a number."
+  exit 1
+fi
+
+# Check if gh (GitHub CLI) is installed
+if ! command -v gh &> /dev/null; then
+  echo "gh (GitHub CLI) could not be found. Please install it from https://github.com/cli/cli."
+  exit 1
+fi
+
+TAG=$1
+VERSION=$2
+RELEASE_EXISTS=$(if gh release view "${TAG}-${VERSION}"; then echo 0; else echo 1; fi)
+
+# Create the release if it doesn't exist
+if [ -n "$RELEASE_EXISTS" ]; then
+    echo "Creating release... (TAG: $TAG | VERSION: $VERSION)"
+    gh release create "${TAG}-${VERSION}" "${TAG}.zip" -t "${TAG} v${VERSION}" -n "Downloaded ${TAG} for VERSION ${VERSION}."
 else
-    echo "Release $tag-$version already exists. Skipping creation."
+    echo "Release ${TAG}-${VERSION} already exists. Skipping creation."
 fi
