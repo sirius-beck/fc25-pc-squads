6038c774d6115573de71a34e55514a1e0f8f6510
Sirius <sirius.red@outlook.com>
2025-01-03 09:10:58 -0300

    feat(scripts): enhance squad processing and versioning
    Updated squad processing script to improve versioning and
    file handling. Key changes:
    - Added function to process both squad types (FUT and regular)
    - Implemented version comparison to avoid unnecessary updates
    - Included timestamp in version file for better tracking
    - Improved error handling and directory checks
    - Streamlined zip file creation process

--- scripts/process-squads.sh
+++ scripts/process-squads.sh
@@ -3,26 +3,40 @@
 RESULT_FOLDER="downloader/result/pc64"
 
 if [ ! -d "$RESULT_FOLDER" ]; then
-  echo "Result folder not found: $RESULT_FOLDER"
-  exit 1
+	echo "Result folder not found: $RESULT_FOLDER"
+	exit 1
 fi
 
 if ! [ -d version ]; then
-  mkdir version
+	mkdir version
 fi
 
-# Process FUT folder
-FUT_SQUADS_FOLDER=$(ls -d $RESULT_FOLDER/FUT/*/)
-FUT_SQUADS_VERSION=$(basename "$FUT_SQUADS_FOLDER")
-FUT_SQUADS_FILES=$(find "$FUT_SQUADS_FOLDER" -type f ! -name "*.bin")
-zip -j fut-squads.zip "$FUT_SQUADS_FILES"
-echo "FUT_SQUADS_VERSION=$FUT_SQUADS_VERSION" >> "$GITHUB_ENV"
-echo "$FUT_SQUADS_VERSION" > version/fut-squads.version
+function processSquads() {
+	local type="$1" # fut-squads | squads
+	local folder_name folder files version_data new_version old_version date_utc
 
-# Process squads folder
-SQUADS_FOLDER=$(ls -d $RESULT_FOLDER/squads/*/)
-SQUADS_VERSION=$(basename "$SQUADS_FOLDER")
-SQUADS_FILES=$(find "$SQUADS_FOLDER" -type f ! -name "*.bin")
-zip -j squads.zip "$SQUADS_FILES"
-echo "SQUADS_VERSION=$SQUADS_VERSION" >> "$GITHUB_ENV"
-echo "$SQUADS_VERSION" > version/squads.version
+	if  [ "$type" != "fut-squads" ] && [ "$type" != "squads" ]; then
+		echo "Invalid type. Please use 'fut-squads' or 'squads'."
+		exit 1
+	fi
+
+	folder_name=$(if [ "$type" == "fut-squads" ]; then echo "FUT"; else echo "squads"; fi)
+	folder=$(ls -d $RESULT_FOLDER/"$folder_name"/*/)
+	files=$(find "$folder" -type f ! -name "*.bin")
+	
+	version_data=$(cat "version/${type}.version" 2>/dev/null)
+	new_version=$(basename "$folder")
+	
+	IFS='|' read -r old_version _ <<< "$version_data"
+	
+	if [ "$new_version" != "$old_version" ]; then
+		date_utc=$(date -u +"%m/%d/%Y %H:%M:%S")
+		
+		echo "${new_version}|${date_utc}" >"version/${type}.version"
+		zip -j "${type}.zip" "$files"
+		echo "Updated ${type} version from ${old_version} to ${new_version}."
+	fi
+}
+
+processSquads "squads"
+processSquads "fut-squads"
