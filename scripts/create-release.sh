05b052f945bdd08c262dbbb69ea4b16c3c5238d8
Sirius Beck <53864824+sirius-beck@users.noreply.github.com>
2025-01-03 05:36:04 -0300

    Update create-release.sh

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-tag = $1 // "squads" or "fut-squads"
-version = $2
+tag=$1 // "squads" or "fut-squads"
+version=$2
 release_exists=$(gh release view $tag-$version --json tagName --jq '.tagName' || echo "not_found")
 
 echo "tag: $tag | version: $version"
-exit 0
 
 if [ "$release_exists" == "not_found" ]; then
     gh release create $tag-$version $tag.zip -t "$tag v$version" -n "Downloaded $tag for version $version."
