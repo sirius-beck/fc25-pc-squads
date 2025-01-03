e3339776238b88ce52ecc826a66c16d09b610421
Sirius Beck <53864824+sirius-beck@users.noreply.github.com>
2025-01-03 05:30:57 -0300

    Update process-squads.sh

--- scripts/process-squads.sh
+++ scripts/process-squads.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-md release-files
-
 # Process FUT folder
 fut_folder=$(ls -d downloader/result/pc64/FUT/*/)
 fut_version=$(basename "$fut_folder")
 fut_files=$(find "$fut_folder" -type f ! -name "*.bin")
-zip -j release-files/fut-squads.zip $fut_files
+zip -j fut-squads.zip $fut_files
 
 # Process squads folder
 squads_folder=$(ls -d downloader/result/pc64/squads/*/)
 squads_version=$(basename "$squads_folder")
 squads_files=$(find "$squads_folder" -type f ! -name "*.bin")
-zip -j release-files/squads.zip $squads_files
+zip -j squads.zip $squads_files
